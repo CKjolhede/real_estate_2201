d2acc7f4485571f3b2612060eeb3316c4eefe715
Chris_K <ckjolhede@gmail.com>
2022-02-09 11:52:10 -0700

    feat: iteration 3 complete

--- lib/house.rb
+++ lib/house.rb
@@ -1,15 +1,38 @@
+require './lib/room'
+require 'pry'
+
 class House
-  attr_reader :price, :address, :rooms
+  attr_reader :price, :address, :rooms, :details, :total_area
 
   def initialize(price, address)
     @price = price
     @address = address
     @rooms = []
+    @details = {'price' => @price, 'address' => @address}
+    @total_area = 0
   end
 
   def add_room(room)
     @rooms << room
   end
 
+  def above_market_value?
+    @price.to_i > 500000
+  end
 
+  def rooms_from_category(category)
+    @rooms.select {|room| room.category == category}
+  end
+
+  def area
+    @rooms.each do |room|
+      @total_area += room.area
+    end
+    @total_area
+  end
+
+  # def price_per_square_foot
+  #   require "pry"; binding.pry
+  #   @price.to_f / self.area
+  # end
 end
--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -3,26 +3,57 @@ require './lib/house'
 require 'rspec'
 
 RSpec.describe House do
+  before(:each) do
+    @house = House.new("$400000", "123 sugar lane")
+    @room_1 = Room.new(:bedroom, 10, '13')
+    @room_2 = Room.new(:bedroom, 11, '15')
+    @room_3 = Room.new(:living_room, 25, '15')
+    @room_4 = Room.new(:basement, 30, '41')
+  end
+
   it "exists and can return attributes" do
-    house = House.new("$400000", "123 sugar lane")
-  expect(house).to be_a(House)
-  expect(house.price).to eq("$400000")
-  expect(house.address).to eq('123 sugar lane')
-  expect(house.rooms).to eq([])
+    expect(@house).to be_a(House)
+    expect(@house.price).to eq("$400000")
+    expect(@house.address).to eq('123 sugar lane')
+    expect(@house.rooms).to eq([])
   end
 
   it 'can add rooms' do
-    room_1 = Room.new(:bedroom, 10, '13')
-    room_2 = Room.new(:bedroom, 11, '15')
-    house.add_room(room_1)
-    house.add_room(room_2)
-    expect(house.rooms).to eq([room_1, room_2])
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    expect(@house.rooms).to eq([@room_1, @room_2])
   end
 
-  it 'can evaluate #above_market_value'
-  expect(house.above_market_value?).to eq(false)
-end
+  it 'can evaluate #above_market_value' do
+    expect(@house.above_market_value?).to eq(false)
+  end
 
-  
+  it 'can sort rooms by category' do
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+    expect(@house.rooms_from_category(:bedroom)).to eq([@room_1, @room_2])
+    expect(@house.rooms_from_category(:basement)).to eq([@room_4])
+  end
 
+  it "can calcluate area of house" do
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    @house.add_room(@room_3)
+    @house.add_room(@room_4)
+    expect(@house.area).to eq(1900)
+  end
+
+  it "can return house details" do
+    expect(@house.details).to be_a(Hash)
+  end
+
+  # it 'can calculate #price_per_square_foot' do
+  #   @house.add_room(@room_1)
+  #   @house.add_room(@room_2)
+  #   @house.add_room(@room_3)
+  #   @house.add_room(@room_4)
+  #   expect(@house.price_per_square_foot).to eq(210.53)
+  # end
 end
